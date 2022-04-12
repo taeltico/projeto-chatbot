26863da48861d943c0326efc3fd55073ecb220fc
taeltico <rafaelwvs16@gmaail.com>
2022-04-12 15:06:21 -0300

    chat bot

---
+++ chatbot.py
@@ -0,0 +1,35 @@
+vencimentomensal=valorhora*horasdetrabalho*dias
+nome="rafael wanderley vitor sa"
+print(valorhora)
+print(dias)
+print(horasdetrabalho)
+print(vencimentomensal)
+print(nome)
+
+numero1=11
+numero2=10
+
+print(numero1+numero2)
+print(numero1*numero2)
+print(numero1/numero2)
+
+float(valorhora)
+int(10.5)
+
+print(nome)
+lista=[1, 9, 5, "rafael", 8, 6, 25]
+
+print(lista)
+
+lista.append(20)
+
+print(lista)
+
+lista.append("python")
+print(lista)
+
+
+lista.index(5)
+lista.count(20)
+print(lista)
+lista.remove(4)
